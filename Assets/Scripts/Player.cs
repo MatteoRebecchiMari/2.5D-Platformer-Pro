2e99b99b7df41337c3649521c53aa0696475464b
Matteo Rebecchi Mari <dev.matteo.rebecchimari@gmail.com>
2020-12-29 11:59:39 +0100

    Added Gravity + Single Jump Logic to the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,26 +25,38 @@ public class Player : MonoBehaviour
     [SerializeField]
     float _gravity = 1.0f;
 
+    [SerializeField]
+    float _jumpHeight = 15.0f;
+
+    float _yMotion = 0f;
+
     void HandleMovements()
     {
         // Get Motion input
         float horizontalMotion = Input.GetAxis("Horizontal");
 
         // Move the X axis (horizontal)
-        Vector3 velocityMotion = Vector3.right * horizontalMotion * Time.deltaTime * _motionSpeed;
+        float xMotion = horizontalMotion * _motionSpeed;
 
-        // Gravity Motion
-        Vector3 gravityMotion = Vector3.zero;
+        // HANDLE Y MOTION (Gravity + jumping)
+
+        // Handle Jumping (only the the character is not on air --> grounded + Hitting Spacebar in keyboard)
+        if (_controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            _yMotion = _jumpHeight;
+        }
 
         // Handle Gravity if the character is NOT GROUNDED
         if (_controller.isGrounded == false)
         {
-            // Apply gravity
-            gravityMotion = Vector3.down * _gravity * Time.deltaTime;
+            // Apply gravity to Y Motion
+            _yMotion -= _gravity;
         }
 
-        // Move the character combining velocity + gravity
-        _controller.Move(velocityMotion + gravityMotion);
+        Vector3 motionVector = new Vector3(xMotion, _yMotion, 0);
+    
+        // Move the character combining all motions
+        _controller.Move(motionVector * Time.deltaTime);
 
     }
 
